d9ec1c105c831acf9405aa985efca22b1c335840
Niepelnosprytny <sebastian.konkol2206@gmail.com>
2023-06-20 22:24:31 +0200

    przesyłanie plików przez formularz działa

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <InsertHotelsForm/>
   </div>
 </template>
+<script setup lang="ts">
+import InsertHotelsForm from "~/components/InsertHotelsForm.vue";
+</script>
--- server/api/hotels/index.post.ts
+++ server/api/hotels/index.post.ts
@@ -1,9 +1,24 @@
-import {Hotel} from "./index.get";
+import { IncomingMessage } from "http";
+import formidable from "formidable";
 
-export default defineEventHandler(async () => {
-    try {
-        return Hotel.find();
-    } catch (e) {
-        console.log(e);
-    }
+export default defineEventHandler(async (event) => {
+    const body = await handleRequest(event.node.req);
+
+    console.log(body);
+
+    return {ok: true};
 });
+
+function handleRequest(req: IncomingMessage) {
+    return new Promise((resolve, reject) => {
+        const form = formidable({ multiples: true })
+        form.uploadDir = "tmp";
+        form.parse(req, (error, fields, files) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve({ ...fields, ...files });
+        });
+    });
+}
