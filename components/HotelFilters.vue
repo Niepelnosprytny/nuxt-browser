e8c4ecb3cf58b2cfbd0c094290c946c9041493ce
Niepelnosprytny <sebastian.konkol2206@gmail.com>
2023-07-05 14:09:15 +0200

    started some basic css

--- components/HotelFilters.vue
+++ components/HotelFilters.vue
@@ -36,48 +36,111 @@ watch(sortBy, async (currentValue) => {
 </script>
 
 <template>
-  <h3>Filters</h3>
-  <select v-model="sortBy">
-    <option value="priceAsc">Price ascending</option>
-    <option value="priceDesc">Price descending</option>
-    <option value="reviewsAsc" selected>Reviews ascending</option>
-    <option value="reviewsDesc">Reviews descending</option>
-  </select>
+  <section>
+    <nav>
+      <div>
+        <label for="sortBy">Sort by: </label>
+        <select v-model="sortBy" id="sortBy">
+          <option value="priceAsc">Price ascending</option>
+          <option value="priceDesc">Price descending</option>
+          <option value="reviewsAsc" selected>Reviews ascending</option>
+          <option value="reviewsDesc">Reviews descending</option>
+        </select>
+      </div>
 
-  <select name="stars" v-model="stars">
-    <option>0</option>
-    <option>1</option>
-    <option>2</option>
-    <option>3</option>
-    <option>4</option>
-    <option>5</option>
-  </select>
+      <div>
+        <label for="stars">Stars: </label>
+        <select v-model="stars" id="stars">
+          <option>0</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
 
-  <label for="minReviewsScore">Mininal reviews score</label>
-  <input v-model="minReviewsScore"
-         type="text"
-         id="minReviewsScore"
-         min="0"
-         step="0.1"
-         oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');">
+      <div>
+        <label for="minReviewsScore">Minimal reviews score</label>
+        <input v-model="minReviewsScore"
+               type="text"
+               id="minReviewsScore"
+               min="0"
+               step="0.1"
+               oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');">
+      </div>
 
-  <label for="breakfast">Breakfast</label>
-  <input v-model="breakfast" type="checkbox" id="breakfast">
-
-  <label for="parking">Parking</label>
-  <input v-model="parking" type="checkbox" id="parking">
-  <br>
-
-  <HotelList v-if="promotedHotels !== []"
-           :hotels="promotedHotels"
-           :breakfast="breakfast"
-           :parking="parking"
-           :stars="stars"
-           :minReviewsScore="minReviewsScore"/>
-  <HotelList v-if="hotels !== []"
-           :hotels="hotels"
-           :breakfast="breakfast"
-           :parking="parking"
-           :stars="stars"
-           :minReviewsScore="minReviewsScore"/>
+      <div class="checkboxList">
+        <h3>Filters</h3>
+        <div class="checkboxDiv">
+          <input v-model="breakfast" type="checkbox" id="breakfast">
+          <label for="breakfast" class="checkboxLabel">Breakfast</label>
+        </div>
+        <div class="checkboxDiv">
+          <input v-model="parking" type="checkbox" id="parking">
+          <label for="parking" class="checkboxLabel">Parking</label>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <HotelList v-if="promotedHotels !== []"
+                 :hotels="promotedHotels"
+                 :breakfast="breakfast"
+                 :parking="parking"
+                 :stars="stars"
+                 :minReviewsScore="minReviewsScore"/>
+      <HotelList v-if="hotels !== []"
+                 :hotels="hotels"
+                 :breakfast="breakfast"
+                 :parking="parking"
+                 :stars="stars"
+                 :minReviewsScore="minReviewsScore"/>
+    </main>
+  </section>
 </template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: row;
+}
+
+main {
+  width: 90%;
+}
+
+nav {
+  display: inline-flex;
+  flex-direction: column;
+  width: 15%;
+  float: left;
+  height: 100%;
+}
+
+select,
+#minReviewsScore {
+  width: 20vh;
+  height: 3vh;
+}
+
+div {
+  margin: 3vh auto;
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.checkboxList {
+  width: 100%;
+}
+
+.checkboxDiv {
+  flex-direction: row;
+  margin: 0.5vh 20%;
+;
+}
+
+input[type="checkbox"]:hover,
+.checkboxLabel:hover {
+  cursor: pointer;
+}
+</style>
